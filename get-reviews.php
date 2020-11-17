b9a139900ec2b9671cbf1319bfe658e1b81aefb5
xl3ehindTim <32740573+xl3ehindTim@users.noreply.github.com>
2020-11-17 16:16:43 +0100

    Update get-reviews.php
    Cache data to api/reviews.json

--- get-reviews.php
+++ get-reviews.php
@@ -1,4 +1,7 @@
 <?php
+$json = file_get_contents("config.json");
+$data = json_decode($json);
+
 /*
 
 💬 Get Google-Reviews with PHP cURL & without API Key
@@ -37,12 +40,12 @@ echo getReviews($options);
 */
 
 $options = array(
-  'google_maps_review_cid' => '17311646584374698221', // Customer Identification (CID)
+  'google_maps_review_cid' => $data->key, // '17311646584374698221', Customer Identification (CID)
   'show_only_if_with_text' => false, // true = show only reviews that have text
   'show_only_if_greater_x' => 0,     // (0-4) only show reviews with more than x stars
   'show_rule_after_review' => true,  // false = don't show <hr> Tag after each review (and before first)
   'show_blank_star_till_5' => true,  // false = don't show always 5 stars e.g. ⭐⭐⭐☆☆
-  'your_language_for_tran' => 'en',  // give you language for auto translate reviews  
+  'your_language_for_tran' => 'nl',  // give you language for auto translate reviews  
   'sort_by_reating_best_1' => true,  // true = sort by rating (best first)
   'show_cname_as_headline' => true,  // true = show customer name as headline
   'show_age_of_the_review' => true,  // true = show the age of each review
@@ -72,18 +75,28 @@ function getReviews($option) {
     $reviews  = json_decode($reviews);                                                                                                                 /* 2. JSON DECODE */
     $customer = $reviews[6][11];                                                                                                                       /* POSITION OF REVIEWS */
     $reviews  = $reviews[6][52][0];                                                                                                                    /* POSITION OF REVIEWS */
-  }                                                                                                                                                    /* END CHECK */
-  $return = '';                                                                                                                                        /* INI VAR */
+  }              
+
+  
+  // Open reviews.json   
+  $fp = fopen('api/reviews.json', 'w');
+                                                                                                                                /* END CHECK */
+  $return = '';       
+                                                                                                                                 /* INI VAR */
   if (isset($reviews)) {                                                                                                                               /* CHECK REVIEWS */
     if ( isset($option['sort_by_reating_best_1']) and $option['your_lansort_by_reating_best_1guage_for_tran'] == true )                                /* CHECK SORT */
       array_multisort(array_map(function($element) { return $element[4]; }, $reviews), SORT_DESC, $reviews);                                           /* SORT */
     $return .= '<div class="quote">';                                                                                                                  /* OPEN DIV */
     if (isset($option['show_cname_as_headline']) and $option['show_cname_as_headline'] == true) $return .= '<strong>'.$customer.'</strong><br>';       /* CUSTOMER */
     if (isset($option['show_rule_after_review']) and $option['show_rule_after_review'] == true) $return .= '<hr size="1">';                            /* RULER */
-    foreach ($reviews as $review) {                                                                                                                    /* START LOOP */
+    foreach ($reviews as $review) {                                                                                                      /* START LOOP */
+      $stars = '';
       if (isset($option['show_only_if_with_text']) and $option['show_only_if_with_text'] == true and empty($review[3])) continue;                      /* CHECK TEXT */
       if (isset($option['show_only_if_greater_x']) and $review[4] <= $option['show_only_if_greater_x']) continue;                                      /* CHECK RATING */
-      for ($i=1; $i <= $review[4]; ++$i) $return .= '⭐';                                                                                              /* RATING */
+      for ($i=1; $i <= $review[4]; ++$i) {
+        $stars .= '⭐';
+        $return .= '⭐';
+      }                                                                                            /* RATING */
       if (isset($option['show_blank_star_till_5']) and $option['show_blank_star_till_5'] == true) for ($i=1; $i <= 5-$review[4]; ++$i) $return .= '☆'; /* RATING */
       $return .= '<br>';                                                                                                                               /* NEWLINE */
       if (isset($option['show_txt_of_the_review']) and $option['show_txt_of_the_review'] == true) $return .= $review[3].'<br>';                        /* TEXT */
@@ -92,8 +105,33 @@ function getReviews($option) {
           isset($option['show_age_of_the_review']) and $option['show_age_of_the_review'] == true) $return .= '<small> &mdash; </small>';               /* PRINT — */
       if (isset($option['show_age_of_the_review']) and $option['show_age_of_the_review'] == true) $return .= '<small>'.$review[1].' </small>';         /* AGE */
       if (isset($option['show_rule_after_review']) and $option['show_rule_after_review'] == true) $return .= '<hr size="1">';                          /* RULER */
-    }                                                                                                                                                  /* END LOOP */
+      // fwrite($fp, json_encode($review[3]));
+      // array_push($array, $review[3]);
+      // echo $return;
+      // $array[] = array(
+      //     'data' => $review[3],
+      // );
+      
+      // echo $array[1];
+      // var_dump($array);
+      // var_dump($review);
+      // echo $review[6];
+      $array[] = array(
+        'name' => $review[0][1],
+        'stars' => $stars,
+        'star_count' => $review[4],
+        'age' => $review[1],
+        'text' => $review[3],
+      );
+      // var_dump($array);
+    } 
+    // echo $return;   
+    fwrite($fp, json_encode($array));
+    fclose($fp);      
+                                                                                                                                                /* END LOOP */
     $return .= '</div>';                                                                                                                               /* CLOSE DIV */
-  }                                                                                                                                                    /* CHECK REVIEWS */
+  }                                                                                                                                                   /* CHECK REVIEWS */
   return $return;                                                                                                                                      /* RETURN DATA */
 }                                                                                                                                                      /* END OF FUNCTION */
+
+// close reviews.json
